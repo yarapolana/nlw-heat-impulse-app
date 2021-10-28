2155a124bdb31020a1d9e966a8a9cf8ac41847fc
Yara Polana <yara.polana@gmail.com>
2021-10-27 20:39:41 -0700

    chore: fetch real time messages

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -2,8 +2,17 @@ import React, { useEffect, useState } from 'react'
 import { ScrollView } from 'react-native'
 
 import { api } from '../../services/api'
+import { socket } from '../../services/socket'
+
 import { Message, IMessage } from '../Message'
 import { styles } from './styles'
+
+const messagesQueue: IMessage[] = []
+
+socket.on('new_message', (newMessage: IMessage) => {
+  messagesQueue.push(newMessage)
+})
+
 export function MessageList() {
   const [messages, setMessages] = useState<IMessage[]>([])
 
@@ -16,6 +25,22 @@ export function MessageList() {
         e instanceof Error && console.warn(JSON.stringify(e.message, null, 2))
       })
   }, []) // when do I want to execute this function, pass variables, each time the variables change it executes this function again
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0], // the oldest message in queue
+          prevState[0],
+          prevState[1]
+        ].filter(Boolean)) // this filter removes messages that are null or undefined
+
+        messagesQueue.shift() // to remove the [0] item in queue giving space to the rest
+      }
+    }, 3000)
+
+    return () => clearInterval(timer)
+  }, [])
 
   return (
     <ScrollView
