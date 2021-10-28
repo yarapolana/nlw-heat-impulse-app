e6e082523a3e036fa8e7bea8bed886f429bec826
Yara Polana <yara.polana@gmail.com>
2021-10-27 20:39:19 -0700

    chore: fetch latest messages

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { ScrollView } from 'react-native'
 
+import { api } from '../../services/api'
 import { Message, IMessage } from '../Message'
 import { styles } from './styles'
 export function MessageList() {
   const [messages, setMessages] = useState<IMessage[]>([])
+
+  useEffect(() => {
+    api.get<IMessage[]>('messages/latest')
+      .then(({ data }) => {
+        setMessages(data)
+      })
+      .catch (e =>  {
+        e instanceof Error && console.warn(JSON.stringify(e.message, null, 2))
+      })
+  }, []) // when do I want to execute this function, pass variables, each time the variables change it executes this function again
 
   return (
     <ScrollView
