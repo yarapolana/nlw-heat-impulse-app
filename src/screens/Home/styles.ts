a2da2a9d921e2c69033e9c2548f0a6434c0ab0ab
Yara Polana <yara.polana@gmail.com>
2021-10-27 20:32:03 -0700

    chore: remove unused

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -5,8 +5,6 @@ import { COLORS } from '../../theme'
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: COLORS.BLACK_SECONDARY,
     paddingTop: getStatusBarHeight() + 17,
   },
