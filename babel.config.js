b11e83a492e87a3b5d36a3d98fe3b1571ff220bc
Yara Polana <yara.polana@gmail.com>
2021-10-27 20:35:17 -0700

    chore: add and config libs with babel

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['inline-dotenv'],
+      ['react-native-reanimated/plugin']
+    ]
   };
 };
