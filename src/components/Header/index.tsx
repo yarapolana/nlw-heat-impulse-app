3c71661d9036c92d4171ef6d8af8f7bd3a577555
Yara Polana <yara.polana@gmail.com>
2021-10-29 14:29:13 -0700

    👩🏽‍💻: add user and signOut to header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,22 +2,27 @@ import React from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
 
 import LogoSvg from '../../assets/logo.svg'
+import { useAuth } from '../../hooks/useAuth'
 import { UserAvatar } from '../UserAvatar'
 import { styles } from './styles'
 
 export function Header() {
+  const {user, signOut} = useAuth()
+
   return (
     <View style={styles.container}>
       <LogoSvg />
 
       <View style={styles.logoutButton}>
-        <TouchableOpacity>
-          <Text style={styles.logoutText}>
-            Sign out
-          </Text>
-        </TouchableOpacity>
+        {user && (
+          <TouchableOpacity onPress={signOut}>
+            <Text style={styles.logoutText}>
+              Sign out
+            </Text>
+          </TouchableOpacity>
+        )}
 
-        <UserAvatar uri='https://github.com/yarapolana.png' />
+        <UserAvatar uri={user?.avatar_url} />
       </View>
     </View>
   )
